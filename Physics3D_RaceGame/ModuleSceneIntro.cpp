c5cae60ab28bce5e5ed5ff5cfd6643668db3476d
Victor Chen <36187875+Scarzard@users.noreply.github.com>
2018-12-22 21:31:49 +0100

    Map finished. Needs joints and sensors.

--- Physics3D_RaceGame/ModuleSceneIntro.cpp
+++ Physics3D_RaceGame/ModuleSceneIntro.cpp
@@ -20,18 +20,42 @@ bool ModuleSceneIntro::Start()
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
 
+	//Beggining
 	CreateCircuit(13, 3, 60, 0, 5, 35, 0, { 0, 1, 0 });
+	//U turn
 	CreateCircuit(13, 3, 30, -8.7, 5, 71, -45, { 0, 1, 0 });
 	CreateCircuit(13, 3, 45, -37, 5, 79.5, 90, { 0, 1, 0 });
 	CreateCircuit(13, 3, 45, -72, 5, 65, 45, { 0, 1, 0 });
+	//L
 	CreateCircuit(13, 3, 45, -105, 5, 52.5, 90, { 0, 1, 0 });
-	CreateCircuit(13, 3, 130, -134, 5, 68.5 + 30 + 12.5, 0, { 0, 1, 0 });
-	CreateCircuit(13, 3, 30, -142.75, 5, 97 + 55 + 30, -45, { 0, 1, 0 });
-	CreateCircuit(13, 3, 30, -164, 5, 105.5 + 55 + 30, 90, { 0, 1, 0 });
-	CreateCircuit(13, 3, 45, -190.25, 5, 91.5 + 55 + 30, 45, { 0, 1, 0 });
-	CreateCircuit(13, 3, 45, -204.25, 5, 57.75 + 55 + 30, 0, { 0, 1, 0 });
-	CreateCircuit(13, 3, 45, -190.25, 5, 24 + 55 + 30, -45, { 0, 1, 0 });
+	CreateCircuit(13, 3, 130, -134, 5, 111, 0, { 0, 1, 0 });
+	//360 turn
+	CreateCircuit(13, 3, 30, -142.75, 5, 182, -45, { 0, 1, 0 });
+	CreateCircuit(13, 3, 30, -164, 5, 190.5, 90, { 0, 1, 0 });
+	CreateCircuit(13, 3, 45, -190.25, 5, 175.5, 45, { 0, 1, 0 });
+	CreateCircuit(13, 3, 45, -204.25, 5, 142.75, 0, { 0, 1, 0 });
+	CreateCircuit(13, 3, 45, -190.25, 5, 109, -45, { 0, 1, 0 });
+	//Ramp
 	CreateCircuit(45, 3, 13, -155, 11.5, 97.5, 17.5, { 0, 0, 1 });
+	//After ramp wider circuit for easier landing
+	CreateCircuit(100, 3, 20, -35, 11.5, 97.5, 0, { 0, 1, 0 });
+	//U turn
+	CreateCircuit(50, 3, 13, 21.5, 11.5, 112.5, 90, { 0, 1, 0 });
+	CreateCircuit(30, 3, 13, 30, 11.5, 144, 0, { 0, 1, 0 });
+	CreateCircuit(50, 3, 13, 51.5, 11.5, 125.5, 90, { 0, 1, 0 });
+	//L
+	CreateCircuit(13, 3, 30, 73, 11.5, 107, 90, { 0, 1, 0 });
+	CreateCircuit(13, 3, 30, 94.5, 11.5, 98.5, 0, { 0, 1, 0 });
+	//Zigzag
+	CreateCircuit(13, 3, 45.5, 84, 11.5, 67, 30, { 0, 1, 0 });
+	CreateCircuit(13, 3, 45.5, 84, 11.5, 34.1, -30, { 0, 1, 0 });
+	CreateCircuit(13, 3, 45.5, 84, 11.5, 1.2, 30, { 0, 1, 0 });
+	//U turn
+	CreateCircuit(13, 3, 30, 73.5, 11.5, -30.25, 0, { 0, 1, 0 });
+	CreateCircuit(40, 3, 13, 60, 11.5, -51.75, 0, { 0, 1, 0 });
+	CreateCircuit(60, 3, 13, 23.5, 11.5, -51.75, 0, { 0, 1, 0 });
+	//Last ramp
+	CreateCircuit(13, 3, 35, 0, 13, -30, -5.5, { 1, 0, 0 });
 
 	
 
